5d7da43be085a6cb883a1f7c62e2d6b39e53cbea
hashbrownsCal <calvin.chun.ku@gmail.com>
2021-09-20 19:35:36 -0700

    first commit

---
+++ scripts/main.js
@@ -0,0 +1,52 @@
+// Practice Assignment 1
+// Question 1
+// The variable myNumber should by defined.
+let myNumber;
+// Question 2
+// The variable myNumber should be type number.
+// The variable myNumber should be equal to 5.
+let myNumber = 5;
+// Question 3
+// The variable myOtherNumber should by defined.
+// The variable myOtherNumber should be type number.
+// The variable myOtherNumber should be equal to 5.
+let myOtherNumber = 5;
+// Question 4
+// The variable myTotal should by defined.
+// The variable myTotal should be type number.
+// The variable myTotal should be equal to the product of myNumber and myOtherNumber.‣
+let myTotal = myNumber * myOtherNumber;
+// Question 5
+// The variable myName should by defined.
+// The variable myName should be type string.
+// The variable myCombo should by defined.
+// The variable myCombo should be type string.
+// The variable myCombo should be equal to the sum of myNumber and myOtherNumber.‣
+let myName = "";
+let myCombo = "";
+myCombo = myNumber + myOtherNumber;
+// Question 6
+// The variable myOtherCombo should by defined.
+// The variable myOtherCombo should be type number.
+// The variable myOtherCombo should be equal to the sum of myNumber and myOtherNumber.‣
+let myOtherCombo = myNumber + myOtherNumber;
+// Question 7
+// The variable myRemainder should by defined.
+// The variable myRemainder should be type number.
+// The variable myRemainder should be equal to the remainder of myNumber divided by 5.
+let myRemainder = myNumber % 5;
+// Question 8
+// The variable i should by defined.
+// The variable i should be type number.
+// The variable i should be equal to 2.
+let i = 2;
+// Question 9
+// The variable int should by defined.
+// The variable int should be type number.
+// The variable int should be equal to 4.
+let int = 4;
+// Question 10
+// The variable myBoolean should by defined.
+// The variable myBoolean should be type boolean.
+// The variable myBoolean should be false.
+let myBoolean = false;
